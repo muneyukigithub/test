39aa3769c0d2e9503f6fe43943627d2c10b3a32d
test <example@test.com>
2022-11-08 18:31:31 +0900

    初回

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "@mui/material/Button"
+import image from "./image/helloworld.png";
+
+const App :React.FC = () =>{
+  return <img src={image} height="200px"/>
+}
+
+export default App;
